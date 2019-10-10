89dd3b35a6b53ad6f7b3a91ccef57d6065596cf0
杨宇千 <crupest@outlook.com>
2019-10-10 20:44:31 +0800

    ...

--- src/admin/UserAdmin.tsx
+++ src/admin/UserAdmin.tsx
@@ -1,26 +1,103 @@
 import React from "react";
 import { withStyles, createStyles } from "@material-ui/styles";
-import { CircularProgress, Typography, Card } from "@material-ui/core";
+import {
+  CircularProgress,
+  Typography,
+  Card,
+  IconButton,
+  Icon,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
 import { WithStyles } from "@material-ui/core/styles";
+import axios from "axios";
 
 import { User, UserWithToken } from "../data/user";
+import { apiBaseUrl } from "../config";
 
-function fetchUserList(token: string): Promise<User[]> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve([
-        {
-          username: "hahaha",
-          administrator: true
-        },
-        {
-          username: "xixixi",
-          administrator: false
-        }
-      ]);
-    }, 2000);
-  });
+async function fetchUserList(token: string): Promise<User[]> {
+  const res = await axios.get(`${apiBaseUrl}/users?token=${token}`);
+  return res.data;
 }
+
+const cardStyles = createStyles({
+  card: {
+    margin: "10px",
+    padding: "10px",
+    display: "flex"
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  cardActions: {}
+});
+
+interface UserCardProps extends WithStyles<typeof cardStyles> {
+  user: User;
+}
+
+interface UserCardState {
+  anchor: HTMLElement | null;
+}
+
+class _UserCard extends React.Component<UserCardProps> {
+  state = {
+    anchor: null
+  };
+
+  constructor(props: UserCardProps) {
+    super(props);
+  }
+
+  onMoreClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    this.setState({ anchor: event.currentTarget });
+  };
+
+  onMenuClose = () => {
+    this.setState({ anchor: null });
+  };
+
+  render(): React.ReactNode {
+    const classes = this.props.classes;
+    const user = this.props.user;
+
+    const anchor = this.state.anchor;
+
+    return (
+      <Card key={user.username} classes={{ root: classes.card }}>
+        <div className={classes.cardContent}>
+          <Typography variant="body1">{user.username}</Typography>
+          <Typography
+            variant="caption"
+            color={user.administrator ? "error" : "textPrimary"}
+          >
+            {user.administrator ? "administrator" : "user"}
+          </Typography>
+        </div>
+        <div className={classes.cardActions}>
+          <IconButton>
+            <Icon color="error">delete</Icon>
+          </IconButton>
+          <IconButton onClick={this.onMoreClick}>
+            <Icon>more_vert</Icon>
+          </IconButton>
+          <Menu
+            anchorEl={anchor}
+            keepMounted
+            open={Boolean(anchor)}
+            onClose={this.onMenuClose}
+          >
+            <MenuItem>Change username</MenuItem>
+            <MenuItem>Change password</MenuItem>
+            <MenuItem>Change permission</MenuItem>
+          </Menu>
+        </div>
+      </Card>
+    );
+  }
+}
+
+const UserCard = withStyles(cardStyles)(_UserCard);
 
 const styles = createStyles({
   loadingArea: {
@@ -31,10 +108,6 @@ const styles = createStyles({
   },
   progressBar: {
     margin: "0 10px"
-  },
-  card: {
-    margin: "10px",
-    padding: "10px"
   }
 });
 
@@ -53,8 +126,10 @@ class UserAdmin extends React.Component<UserAdminProps, UserAdminState> {
 
   constructor(props: UserAdminProps) {
     super(props);
+  }
 
-    fetchUserList(props.user.token).then(users => {
+  componentDidMount() {
+    fetchUserList(this.props.user.token).then(users => {
       this.setState({
         users
       });
@@ -66,14 +141,7 @@ class UserAdmin extends React.Component<UserAdminProps, UserAdminState> {
     const users = this.state.users;
     if (users) {
       const userComponents = users.map(user => {
-        return (
-          <Card key={user.username} classes={{ root: classes.card }}>
-            <Typography variant="body1">{user.username}</Typography>
-            <Typography variant="caption">
-              {user.administrator ? "administrator" : "user"}
-            </Typography>
-          </Card>
-        );
+        return <UserCard key={user.username} user={user} />;
       });
 
       return <div>{userComponents}</div>;
