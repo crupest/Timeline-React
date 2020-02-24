68559434f6c86eb156ea510bb1beeba51e804edb
crupest <crupest@outlook.com>
2020-02-24 17:31:35 +0800

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
+    'prettier/react',
     'prettier/@typescript-eslint'
   ],
   globals: {
