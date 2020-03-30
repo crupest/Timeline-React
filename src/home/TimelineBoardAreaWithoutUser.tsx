82c0f13fef9b8677782c7d75f79cb2926049e483
crupest <crupest@outlook.com>
2020-03-30 18:43:58 +0800

    ...

--- src/home/TimelineBoardAreaWithoutUser.tsx
+++ src/home/TimelineBoardAreaWithoutUser.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Spinner, Row, Card } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
-import TimelineBoard from './TimelineBoard';
 import { TimelineInfo } from '../data/timeline';
+import UserTimelineLogo from '../common/UserTimelineLogo';
+import TimelineLogo from '../common/TimelineLogo';
+import { Link } from 'react-router-dom';
 
 interface TimelineBoardAreaWithoutUserProps {
   publicTimelines?: TimelineInfo[];
@@ -12,13 +14,43 @@ interface TimelineBoardAreaWithoutUserProps {
 const TimelineBoardAreaWithoutUser: React.FC<TimelineBoardAreaWithoutUserProps> = props => {
   const { t } = useTranslation();
 
+  const { publicTimelines } = props;
+
+  if (publicTimelines == null) {
+    return (
+      <Row className="justify-content-center">
+        <Spinner color="primary" />
+      </Row>
+    );
+  }
+
   return (
-    <Container className="p-3">
-      <TimelineBoard
-        title={t('home.allTimeline')}
-        timelines={props.publicTimelines}
-      />
-    </Container>
+    <Row className="p-3 justify-content-center">
+      <div className="cru-card container">
+        {publicTimelines.map(timeline => {
+          const { name } = timeline;
+          const isPersonal = name.startsWith('@');
+          return (
+            <div key={name} className="timeline-item">
+              {isPersonal ? (
+                <UserTimelineLogo className="icon" />
+              ) : (
+                <TimelineLogo className="icon" />
+              )}
+              <Link
+                to={
+                  isPersonal
+                    ? `/users/${timeline.owner.username}`
+                    : `/timelines/${t.name}`
+                }
+              >
+                {name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </Row>
   );
 };
 
