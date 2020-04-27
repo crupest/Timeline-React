a60fc5331a178bd7bbd5bf4a050c6b110a2e29ec
crupest <crupest@outlook.com>
2020-04-27 23:29:59 +0800

    Add pepjs.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import 'regenerator-runtime';
 import 'core-js/modules/es.promise';
 import 'core-js/modules/es.array.iterator';
+import 'pepjs';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
